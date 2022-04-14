71ca24710999c677d74d0f9d5e0f0d7abdb8c26c
Abdulsalam-Baruwa <abdulsalam.baruwa190541028@st.lasu.edu.ng>
2022-04-14 04:30:36 +0100

    BMI calculator 2.0

---
+++ Day 3/Day 3.2-BMI calculator 2.0/main.py
@@ -0,0 +1,19 @@
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg: "))
+
+bmi = round(weight / height ** 2)
+
+if bmi < 18.5:
+    print(f"Your bmi is {bmi}, and you are underweight.")
+
+elif bmi < 25:
+    print(f"Your bmi is {bmi}, and you have a normal weight.")
+
+elif bmi < 30:
+    print(f"Your bmi is {bmi}, and you are overweighted.")
+
+elif bmi < 35:
+    print(f"Your bmi is {bmi}, and you are OBESE.")
+
+else:
+    print(f"Your bmi is {bmi}, and you are clinically obese.")
