2afa43bb8b0ad9aca12665b6e2d68c89bccdd0c4
Abdulsalam-Baruwa <abdulsalam.baruwa190541028@st.lasu.edu.ng>
2022-04-17 12:13:39 +0100

    changes

--- Day 3/Day 3.4 - pizza order/main.py
+++ Day 3/Day 3.4 - pizza order/main.py
@@ -1,24 +1,24 @@
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size of pizza do you want? S, M, or L:\n")
-add_pepperoni = input("Do you want pepperoni? Y or N \n")
-extra_cheese = input("Do you want extra cheese? Y or N \n")
+size = input("What size of pizza do you want? S, M, or L:\n").lower()
+add_pepperoni = input("Do you want pepperoni? Y or N \n").lower()
+extra_cheese = input("Do you want extra cheese? Y or N \n").lower()
 
 bill = 0
 
-if size == "S":
+if size == "s":
     bill += 15
-elif size == "M":
+elif size == "m":
     bill += 20
 else:
     bill += 25
 
-if add_pepperoni == "Y":
-    if size == "S":
+if add_pepperoni == "y":
+    if size == "s":
         bill += 2
     else:
         bill += 3
 
-if extra_cheese == "Y":
+if extra_cheese == "y":
     bill += 1
 
 print(f"Your final bill is: ${bill}.")
