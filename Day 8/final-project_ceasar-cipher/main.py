d1750d62eece2a028bcaae69244535ad2095cc9c
Abdulsalam-Baruwa <abdulsalam.baruwa190541028@st.lasu.edu.ng>
2022-05-01 16:30:57 +0100

    tidied my code

--- Day 8/final-project_ceasar-cipher/main.py
+++ Day 8/final-project_ceasar-cipher/main.py
@@ -1,6 +1,3 @@
-from turtle import position
-
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
@@ -23,37 +20,9 @@ while go_again == True:
     shift = int(input("Type the shift number:\n"))
     shift = shift % 25
 
-
     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
     again = input("Type 'yes' if you want to go again, Otherwise, type 'no'.\n").lower()
     if again == 'no':
         go_again = False
         print("Goodbye 👋")
-
-'''def encrypt(plain_text, shift_amount):
-    encrypted_text = []
-
-    for letter in plain_text:
-        position = alphabet.index(letter) + shift_amount
-
-        encrypted_text.append(alphabet[position])
-    print(f"The encoded text is: {''.join(encrypted_text)}")
-
-def decrypt(cipher_text, shift_amount):
-    decrypted_text = []
-
-    for letter in cipher_text:
-        position = alphabet.index(letter) - shift_amount
-        
-        decrypted_text.append(alphabet[position])
-    print(f"The decoded text is: {''.join(decrypted_text)}")
-
-if direction == 'encode':
-
-    #keyword argument
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == 'decode':
-    decrypt(cipher_text=text, shift_amount=shift)
-
-'''
