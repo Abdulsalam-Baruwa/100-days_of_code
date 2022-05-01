8cad687837ed6271982e96b9592f407dd6d0e3d2
Abdulsalam-Baruwa <abdulsalam.baruwa190541028@st.lasu.edu.ng>
2022-05-01 18:37:10 +0100

    Grading program

--- Day 9/Day-9.1_Grading-program/main.py
+++ Day 9/Day-9.1_Grading-program/main.py
@@ -1,3 +1,6 @@
+from turtle import st
+
+
 student_scores = {
     "Harry": 81,
     "Ron": 78,
@@ -8,10 +11,20 @@ student_scores = {
 # Create an empty dictionary called student_grades
 student_grades = {}
 # Add the grades to student_grades
-student_grades["Harry"] = "Exceeds Expectations"
-student_grades["Ron"] = "Acceptable"
-student_grades["Hermione"] = "Outstanding"
-student_grades["Draco"] = "Acceptable"
-student_grades["Neville"] = "Fail"
+#student_grades["Harry"] = "Exceeds Expectations"
+#student_grades["Ron"] = "Acceptable"
+#student_grades["Hermione"] = "Outstanding"
+#student_grades["Draco"] = "Acceptable"
+#student_grades["Neville"] = "Fail"
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80 and score < 90:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70 and score < 80:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
 
 print(student_grades)
